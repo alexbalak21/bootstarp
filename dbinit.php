b62856d0e9575f5a958e6f376b82ac7b3f2dc2b1
Alex Balak <alex.balak@outlook.com>
2021-05-20 13:46:38 +0200

    upadte sql instructions

--- dbinit.php
+++ dbinit.php
@@ -21,12 +21,11 @@ function db_connect()
     }
 }
 
-
 function create_table_users()
 {
     db_connect();
     global $pdo;
-    $sql = "CREATE TABLE users (
+    $sql = "CREATE TABLE `users` (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(50) NOT NULL UNIQUE,
     firstname VARCHAR(50) NOT NULL,
@@ -36,7 +35,7 @@ function create_table_users()
     reg_date DATE NULL DEFAULT NULL,
     validated BOOLEAN DEFAULT 0,
     validation VARCHAR(255) NULL DEFAULT NULL,
-    activated BOOLEAN DEFAULT 0
+    activated BOOLEAN DEFAULT 1
     )";
     $pdo->exec($sql);
     echo "TABLE USERS CREATED SUCCESSFULLY";
@@ -49,8 +48,9 @@ function create_table_events()
     global $pdo;
     $sql = "CREATE TABLE `events` (
     `eventID` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `creatorID` INT(6) NOT NULL,
-    `name` VARCHAR(150) NULL DEFAULT NULL ,
+    `creatorID` INT(6) UNSIGNED,
+     FOREIGN KEY (creatorID) REFERENCES users(id),
+    `name` VARCHAR(150) NULL DEFAULT NULL,
     `category` VARCHAR(50) NULL DEFAULT NULL,
     `place`  VARCHAR(255) NULL DEFAULT NULL,
     `city` VARCHAR(50) NULL DEFAULT NULL ,
@@ -60,14 +60,28 @@ function create_table_events()
     `postDate` DATE NULL DEFAULT NULL,
     `startTime` DATETIME NULL DEFAULT NULL,
     `subscribed` INT(2) DEFAULT 0
-    
+
     )";
     $pdo->exec($sql);
     echo "<br>TABLE EVENTS CREATED SUCCESSFULLY";
     $pdo = null;
 }
 
+function create_table_sub()
+{
+    db_connect();
+    global $pdo;
+    $sql = "CREATE TABLE `subs` (
+    `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `eventID` INT(6) UNSIGNED,
+    FOREIGN KEY (eventID) REFERENCES events(eventID) ON DELETE CASCADE,
+    `userID` INT(6) UNSIGNED,
+    FOREIGN KEY (userID) REFERENCES users(id) ON DELETE CASCADE,
+    `subTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    )";
+    $pdo->exec($sql);
+    echo "<br>TABLE SUB CREATED SUCCESSFULLY";
+    $pdo = null;
+}
 
-create_table_events();
-
-?>
+create_table_sub();
