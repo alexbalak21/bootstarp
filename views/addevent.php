91b019f87162065facd50dca3a361a7d03732f68
Alex <alex.balak.dev@yandex.com>
2021-05-20 11:47:15 +0200

    udate

---
+++ views/addevent.php
@@ -0,0 +1,46 @@
+<main class="container my-5">
+  <h2>Ajouter un evenement:</h2>
+  <form class="row g-3 needs-validation" action="components/controller.php" method="POST" novalidate>
+  <img class="col-12 mb-4" src="assets/calendar2-event.svg" alt="person LOGO" width="72" height="57">
+
+    <input type="file" name="fileUpload" class="form-control-file">
+
+    <div class="col-md-8 py-4 text-center">
+      <label class="form-label ">Nom de l'evenment:</label>
+      <input type="text" name="eventName" class="form-control" required />
+    </div>
+    <div class="col-md-8 py-4 text-center">
+      <label class="form-label ">Nom de l'evenment:</label>
+      <input type="text" name="eventName" class="form-control" required />
+    </div>
+
+<div class="col-md-8">
+<label for="">Description:</label>
+    <textarea class="form-control"rows="4"></textarea>
+</div>
+<div class="col-md-8 py-4 text-center">
+      <label class="form-label ">Ville:</label>
+      <input type="text" name="city" class="form-control" required />
+    </div>
+    <div class="col-md-8 py-4 text-center">
+      <label class="form-label ">Lieu Exacte:</label>
+      <input type="text" name="place" class="form-control" required />
+    </div>
+    <div class="col-md-8 py-4 text-center">
+      <label class="form-label ">Nom de l'evenment:</label>
+      <input type="text" name="eventName" class="form-control" required />
+    </div>
+    <div class="col-md-8 py-4 text-center">
+      <label class="form-label ">Date et Heure:</label>
+      <input type="datetime-local" name="eventName" class="form-control" required />
+    </div>
+    <div class="col-md-8 py-4 text-center">
+      <label class="form-label ">Nom de l'evenment:</label>
+      <input type="text" name="eventName" class="form-control" required />
+    </div>
+    <div class="col-md-8 py-4 text-center">
+      <label class="form-label ">Nom de l'evenment:</label>
+      <input type="text" name="eventName" class="form-control" required />
+    </div>  
+  </form>
+</main>
