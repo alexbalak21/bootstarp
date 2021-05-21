89c1c5f854a63f3573e23ccf8e1f235ab47154c2
Alex Balak <alex.balak@outlook.com>
2021-05-22 00:54:04 +0200

    update

--- blocks/navbar.php
+++ blocks/navbar.php
@@ -1,7 +1,7 @@
 <!-- NAVBAR -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="index.php">
       <svg xmlns="http://www.w3.org/2000/svg" width="1.25rem" height="1.25rem" fill="currentColor" viewBox="0 0 16 16">
         <path
           d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"
@@ -26,7 +26,7 @@
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="?page=home">
+          <a class="nav-link" href="?page=?page=events">
             Home
             <span class="sr-only">(current)</span>
           </a>
@@ -34,9 +34,10 @@
         <li class="nav-item"><a class="nav-link" href="?page=login">Login</a></li>
         <li class="nav-item"><a class="nav-link" href="?page=signin">Inscription</a></li>
         <li class="nav-item"><a class="nav-link" href="?page=profile">Profile</a></li>
-        <li class="nav-item"><a class="nav-link" href="?page=activity">Activite</a></li>
+        <li class="nav-item"><a class="nav-link" href="?page=event&nbr=1">Event</a></li>
         <li class="nav-item"><a class="nav-link" href="?page=addevent">AJOUTER EVENT</a></li>
         <li class="nav-item"><a class="nav-link" href="?page=events">Voir les evenements</a></li>
+        <li class="nav-item"><a class="nav-link" href="?logout">Log Out</a></li>
       </ul>
     </div>
   </div>
--- components/controller.php
+++ components/controller.php
@@ -2,16 +2,16 @@
 require_once "model.php";
 require_once "upload.php";
 
-
 //-----------------------------------------------REGISTER
 if (isset($_POST['register'])) {
     $img = 'profile.png';
     if (!empty($_FILES['fileToUpload']['name'])) {
         $error = imgFileUpload();
-        if($error == 0)
-        $_GET['uploadError']= $error;
-        else
-        $img = $_FILES['uploaded'];
+        if ($error == 0) {
+            $_GET['uploadError'] = $error;
+        } else {
+            $img = $_FILES['uploaded'];
+        }
     }
     $email = $_POST['email'];
     $password1 = $_POST['password1'];
@@ -26,7 +26,7 @@ if (isset($_POST['register'])) {
     }
 }
 
-//----------------------------------------------LOGIN
+//-----------------------------------------------------------------LOGIN
 if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -52,55 +52,114 @@ function set_cookie($name, $value, $expDays)
     return $set;
 }
 
-//--------------------------------------------ADD PRODUCT
-$user = json_decode($_COOKIE['user'], true);
-
-// Array ( [name] => Apple Iphone [category] => Phone [price] => 100 [description] => Some text... [addProduct] => [productForm] => REGISTER
-if (isset($_POST['addProduct'])) {
-    $img = 'product.png';
+//-------------------------------------------------------------------------ADD EVENT
+if (isset($_POST['addEvent'])) {
+    if (isset($_COOKIE['user'])) {
+        $USER = json_decode($_COOKIE['user'], true);
+    } else {
+        header("Location: index.php");
+    }
+    $userID = $USER['id'];
+    $img = 'default.png';
     if (!empty($_FILES['fileToUpload']['name'])) {
         $error = imgFileUpload();
-        $img = $_FILES['uploaded'];
-    }
-    $lastID = addProduct($user['id'], $_POST['name'], $_POST['category'], $_POST['price'], $_POST['description'], $img);
-    if ($lastID > 0) {
-        header("Location: ../index.php?page=login");
-    }
-}
-
-if (isset( $_POST['addEvent'])){
-    // $user = json_decode($_COOKIE['user'], true);
-    $userID = 12;
-        $img = 'default.png';
-        if (!empty($_FILES['fileToUpload']['name'])) {
-            $error = imgFileUpload();
-            if($error == 0)
-            $_GET['uploadError']= $error;
-            else
+        if ($error == 0) {
+            $_GET['uploadError'] = $error;
+        } else {
             $img = $_FILES['uploaded'];
         }
-    $user['id'] = 12;
+    }
     $name = $_POST['eventName'];
     $category = $_POST['category'];
     $place = $_POST['place'];
     $city = $_POST['city'];
     $description = $_POST['description'];
-    $startTime = $_POST['startTime'];
-    $latsID = addEvent($userID, $name, $category, $place, $city, $description, $startTime, $img);
-    if($latsID > 0){
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $latsID = addEvent($userID, $name, $category, $place, $city, $description, $date, $time, $img);
+    if ($latsID > 0) {
         header("Location:../index.php");
     }
 }
+//-------------------------------------------------------------------------UPDATE EVENT
+if (isset($_POST['updateEvent'])) {
+    if (isset($_COOKIE['user'])) {
+        $USER = json_decode($_COOKIE['user'], true);
+    } else {
+        header("Location: index.php");
+    }
+    $userID = $USER['id'];
+    $img = 'default.png';
+    if (!empty($_FILES['fileToUpload']['name'])) {
+        $error = imgFileUpload();
+        if ($error == 0) {
+            $_GET['uploadError'] = $error;
+        } else {
+            $img = $_FILES['uploaded'];
+        }
+    }
+    $eventID = $_POST['id'];
+    $name = $_POST['eventName'];
+    $category = $_POST['category'];
+    $place = $_POST['place'];
+    $city = $_POST['city'];
+    $description = $_POST['description'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $updated = updateEvent($eventID, $name, $category, $place, $city, $description, $date, $time, $img);
+    if ($updated) {
+        header("Location:../index.php");
+    } else {
+        $_GET['message'] = 'EVENT UPDATE FAILED';
+    }
+    return null;
+}
 
-if(isset($_GET['admin'])){
+if (isset($_GET['admin'])) {
     //CHECK IF ADMIN
     $admin = $_GET['admin'];
-    if($admin == 'allevents'){
+    if ($admin == 'allevents') {
         echo "TETS";
     }
 }
-
-function getAllEvents(){
-    $events = getAll('events');
-    return $events;
+//-------------------------------------------UPDATE USER
+if (isset($_POST['updateUser'])) {
+    if (isset($_COOKIE['user'])) {
+        $USER = json_decode($_COOKIE['user'], true);
+    } else {
+        header("Location: index.php");
+    }
+    $img = $USER['img'];
+    if (!empty($_FILES['fileToUpload']['name'])) {
+        $error = imgFileUpload();
+        if ($error == 0) {
+            $_GET['uploadError'] = $error;
+        } else {
+            $img = $_FILES['uploaded'];
+        }
+    }
+    $email = $_POST['email'];
+    $password2 = $_POST['password2'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $update = updateUser($USER['id'], $email, $firstname, $lastname, $password2, $img);
+    $pdo = null;
+    if ($update) {
+        header("Location: ../index.php?page=login&update=success");
+    } else {
+        $_GET['message'] = "Update Error";
+    }
+    return null;
+}
+//--------------------------------------------------DELETE EVENT
+if (isset($_POST['deleteEvent'])) {
+    if (isset($_COOKIE['user'])) {
+        $USER = json_decode($_COOKIE['user'], true);
+    } else {
+        header("Location: index.php");
+    }
+    $eventID = $_POST['id'];
+    $done = deleteEvent($eventID, $USER['id']);
+    var_dump($done);
+    die;
 }
