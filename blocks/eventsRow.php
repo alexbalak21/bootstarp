90e33b319209cbb536928f477a2a73a3d78cc9d7
Alex Balak <alex.balak@outlook.com>
2021-05-24 14:30:14 +0200

    updated

--- blocks/eventsRow.php
+++ blocks/eventsRow.php
@@ -1,15 +1,14 @@
 <tr>
 <td><?=$event['id'] ?></td>
-<td><?=$event['name'] ?></td>
+<td><a href="?page=event&id=<?=$eventID ?>"><?=$event['name'] ?></a></td>
 <td><?=$event['category'] ?></td>
 <td><?=$event['city'] ?></td>
 <td><?=$event['place'] ?></td>
 <td><?=$date ?></td>
 <td><?=$time ?></td>
 <td><?=$postDate ?></td>
-<td><a href="index.php?page=lestEventUsers&eventID=<?=$id ?>"><?=$event['subscribed'] ?></a></td>
+<td><a href="index.php?page=lestEventUsers&eventID=<?=$eventID ?>"><?=$count ?></a></td>
 <td><?=$activ ?></td>
 <td><?=$delete ?></td>
 <td><?="<img class='tab-img' src='public/uploads/$img'></img>" ?></td>
 </tr>
-
