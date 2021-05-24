97b04ad1531c39797810ea82fc158c890976a211
Alex Balak <alex.balak@outlook.com>
2021-05-24 03:29:36 +0200

    upade

---
+++ blocks/eventsTableRow.php
@@ -0,0 +1,26 @@
+<tr>
+  <td><a href="?page=event&id=<?=$eventID ?>">
+  <a href="?page=event&id=<?=$eventID ?>"><img class='tab-big-img' src="public/uploads/<?=$img ?>" alt="<?=$img ?>"></a>
+  </td>
+  <td>
+  <?=$date ?>
+  </td>
+  <td class="">
+  <a class="" href="?page=event&id=<?=$eventID ?>"><?=$time ?></a>
+  </td>
+  <td>
+  <a href="?page=event&id=<?=$eventID ?>"><?=$name ?></a>
+  </td>
+  <td>
+  <a href="?page=event&id=<?=$eventID ?>"> <?=$city ?></a>
+  </td>
+  <td>
+  <a href="?page=event&id=<?=$eventID ?>"> <?=$place ?></a>
+  </td>
+  <td>
+  <a href="?page=event&id=<?=$eventID ?>"><?=$subs ?></a>
+  </td>
+  <td>
+  <a href="?page=event&id=<?=$eventID ?>"><button class="btn btn-primary">Participer</button></a>
+  </td>
+</tr>
