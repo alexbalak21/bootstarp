8cb7340e81d70f19f942adbc45dfd9859a02f0aa
Alex Balak <alex.balak@outlook.com>
2021-05-28 05:09:25 +0200

    update

--- js/signin.js
+++ js/signin.js
@@ -1,13 +1,14 @@
-document.getElementById("formLogo").addEventListener("click", () => {
-  document.getElementById("fileUpload").click()
+document.getElementById('formLogo').addEventListener('click', () => {
+  document.getElementById('fileUpload').click()
 })
-document.getElementById("registerUser").addEventListener("submit", (event) => {
-  let pass1 = document.getElementsByName("password1")[0].value
-  let pass2 = document.getElementsByName("password2")[0].value
-  if (pass1 !== pass2 || pass1 == "" || pass2 == "") {
+
+document.getElementById('registerUser').addEventListener('submit', (event) => {
+  let pass1 = document.getElementsByName('password1')[0].value
+  let pass2 = document.getElementsByName('password2')[0].value
+  if (pass1 !== pass2 || pass1 == '' || pass2 == '') {
     event.preventDefault()
-    document.getElementById("error").innerHTML = "Les champs ne sont pas correctement remplis."
-    document.getElementsByName("password1")[0].classList.add("is-invalid")
-    document.getElementsByName("password2")[0].classList.add("is-invalid")
-  } else document.getElementById("registerUser").submit()
+    document.getElementById('error').innerHTML = 'Les champs ne sont pas correctement remplis.'
+    document.getElementsByName('password1')[0].classList.add('is-invalid')
+    document.getElementsByName('password2')[0].classList.add('is-invalid')
+  } else document.getElementById('registerUser').submit()
 })
--- views/profile.php
+++ views/profile.php
@@ -3,13 +3,12 @@ require_once "components/profile_controller.php";
 ?>
 <main id='main' class="mt-4 container">
   <div class="row">
+
     <div class="col-12 row border">
-
     <h1 class="text-center my-4 col-lg-10">Votre Compte</h1>
-
-    <div class="col-md-3">
+      <div class="col-md-3">
       <a href="#!"><img class="card-img-top" src="public/uploads/<?=$USER['img'] ?>" alt="..." /></a>
-    </div>
+      </div>
     <div class="col-md-9 text-center py-5">
       <h2><?=$USER['firstname'] ?></h2>
       <br />
@@ -17,20 +16,23 @@ require_once "components/profile_controller.php";
       <br />
       <h2><?=$USER['email'] ?></h2>
       <br />
-    <div>
-    <a href="?page=updateProfile"><button class="btn btn-lg btn-primary" type="submit">Modifer</button></a>
+      <div>
+          <a href="?page=updateProfile"><button class="btn btn-lg btn-primary" type="submit">Modifer</button></a>
+      </div>
     </div>
     </div>
-    </div>
+
     <div class="col-12 row text-center">
     <div class="my-5">
       <div class="py-3 row ">
+
         <h2 class="px-4 col-lg-5">Vos activitées à venir:</h2>
         <a class="col-lg-2" href="?page=eventsTable&userID=<?=$userID ?>"><h2><?=$MyEvents ?></h2>
         <div class="col-lg-2">
         <a class="col-lg-2" href="?page=eventsTable&userID=<?=$userID ?>"><button type="button" class="btn-lg btn-primary">Voir</button></a>
-        </div>
       </div>
+      </div>
+
       <div class="py-3 row ">
         <h2 class="px-4 col-lg-5">Vos activitées à participées:</h2>
         <a class="col-lg-2" href="index.php?page=userSubEvntsTable&id=<?=$userID ?>"><h2><?=$participatingEvents ?></h2></a>
@@ -41,5 +43,7 @@ require_once "components/profile_controller.php";
       </div>
       </div>
       </div>
+    </div>
+    </div>
     </div>
 </main>
